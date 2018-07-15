8872d3ff69f7a767a1085f38b965a6660f6842fb
Natasha K <kapr.natasha@gmail.com>
2018-07-15 19:42:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    string = "The line is currently:"
+    string = "The line is currently: "
     katz_deli.each do |client|
       counter = 1
       string.push("#{counter}. #{client}")
